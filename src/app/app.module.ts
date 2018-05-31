d9ade9b88caf9890db3ef1f24ac3737d74c86acc
Radoslaw Kaczmarek <radek_kaczmarek@arcor.de>
2018-05-31 15:47:01 +0200

    2. Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
+import { ProductPageComponent } from './product-page/product-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductPageComponent,
-    ProductDescriptionComponent
+    ProductDescriptionComponent,
+    ProductPageComponent
   ],
   imports: [
     BrowserModule,

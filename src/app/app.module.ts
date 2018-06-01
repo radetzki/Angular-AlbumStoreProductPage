c14415f73bd46f68c1054ef0469627bdcaa9bc45
Radoslaw Kaczmarek <radek_kaczmarek@arcor.de>
2018-06-01 21:10:33 +0200

    Adding the tracklisting component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductService } from './product.service';
+import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductDescriptionComponent,
-    ProductPageComponent
+    ProductPageComponent,
+    ProductTracklistingComponent
   ],
   imports: [
     BrowserModule,
